ae5d4bcde50483bb3009bf983842e37271e7de0c
da.smirnov <da.smirnov@yadro.com>
2026-03-12 22:21:59 +0300

    Fix nixos

--- configuration.nix
+++ configuration.nix
@@ -103,9 +103,19 @@
 
   nix.settings = {
     show-trace = true;
-    extra-experimental-features = [ "nix-command" "flakes" ];
-    extra-sandbox-paths = [];
     show-progress = true;
+    max-jobs = 4;
+    extra-experimental-features = [ "nix-command" "flakes" ];
+
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   # Open ports in the firewall.
