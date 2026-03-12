08fedbfab5f6346d557cee06313d0d72a21e5fdd
Daniil <107540177+xm1k@users.noreply.github.com>
2026-03-12 16:00:37 +0000

    disko

---
+++ disko-config.nix
@@ -0,0 +1,39 @@
+{
+  disko.devices = {
+    disk.main = {
+      device = "/dev/nvme0n1";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+
+          swap = {
+            size = "8G";
+            content = {
+              type = "filesystem";
+              format = "swap";
+            };
+          };
+
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+  };
+}
