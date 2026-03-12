1e9e9c8efc40b5486ba1d8839f814f3081bb471d
da.smirnov <da.smirnov@yadro.com>
2026-03-13 00:57:09 +0300

    Add home manage

--- flake.nix
+++ flake.nix
@@ -3,16 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
-  {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ваше_имя_пользователя = import ./home.nix;
+        }
       ];
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
     };
   };
 }
