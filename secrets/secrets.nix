eeb1953758512da3a41e229b512b7294a1d1f269
da.smirnov <da.smirnov@yadro.com>
2026-03-13 19:20:46 +0300

    f

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,5 @@ let
   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA80EZzkNzIOn6o1Pr1p6uzwifnP9uEjZu0o1/zJKDRU xm1k@nixos;
 in
 {
-  "openai.age".publicKeys = [ xm1k ];
-  "vpn.token.age".publicKeys = [ xm1k ];
+  "password.age".publicKeys = [ xm1k ];
 }
