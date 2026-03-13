a38f882dbabd413162ff8d636818f1631fafb37b
da.smirnov <da.smirnov@yadro.com>
2026-03-13 19:31:01 +0300

    f

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,6 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./desktop/niri/niri.nix
-      ./secrets/secrets.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
