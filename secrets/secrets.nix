066fefb3df42d6e6753a8df6d5c73a5f998c2be8
da.smirnov <da.smirnov@yadro.com>
2026-03-13 19:27:17 +0300

    f

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,7 @@
 let
-  # Публичный SSH‑ключ пользователя xm1k
   xm1k = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA80EZzkNzIOn6o1Pr1p6uzwifnP9uEjZu0o1/zJKDRU xm1k@nixos";
+  users = [ xm1k ];
 in
 {
-  # Для каждого файла .age указываем массив публичных ключей
   "password.age".publicKeys = [ xm1k ];
 }
