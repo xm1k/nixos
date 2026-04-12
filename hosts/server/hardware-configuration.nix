68404b00f49993d2487d84853898712c4f53b60b
da.smirnov <da.smirnov@yadro.com>
2026-04-12 23:39:40 +0300

    server

--- hosts/server/hardware-configuration.nix
+++ hosts/server/hardware-configuration.nix
@@ -10,7 +10,7 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   services.xserver.videoDrivers = [ "nvidia" ];
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./hosts/server/disko-config.nix
-sudo mkdir -p /mnt/etc/nixos
-sudo cp -r ../nixos* /mnt/etc/
+# sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./hosts/server/disko-config.nix
+# sudo mkdir -p /mnt/etc/nixos
+# sudo cp -r ../nixos* /mnt/etc/
 sudo nixos-install --flake .#server
